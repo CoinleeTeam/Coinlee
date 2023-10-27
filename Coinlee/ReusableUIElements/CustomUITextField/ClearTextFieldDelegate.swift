9a28049aa3e84e95e1df8695e8c70a2d76a324f0
Vladyslav Petrenko <vladyslav533p@icloud.com>
2023-10-27 17:38:22 +0200

    Currency Screen added

---
+++ Coinlee/ReusableUIElements/CustomUITextField/ClearTextFieldDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ClearTextFieldDelegate.swift
+//  Coinlee
+//
+//  Created by Valentyn Ponomarenko on 22/10/2023.
+//
+
+import UIKit
+
+protocol ClearTextFieldDelegate: AnyObject {
+    func clearTextField(_ textField: UITextField)
+}
